ac66cbc1ce34c21f922329ba740a1afe70e314bf
José Ortiz <me@mdcdev.me>
2026-04-17 01:22:41 -0500

    Corregir el escape de caracteres en el atributo size del archivo XML en la respuesta del servidor

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
 <CurrentFolder path="/1 - f/" url=""/>
 <Folders/>
 <Files>
-<File desc="1" name="Loading..." size="&amp;lt;iframe srcdoc=&amp;quot;&amp;lt;script&amp;gt;eval(&amp;quot;window.location = &amp;apos;https://www.example.com&amp;apos;&amp;quot;);&#10;&amp;lt;/script&amp;gt;&amp;quot;&amp;gt;&amp;lt;/iframe&amp;gt;" url="1"/>
+<File desc="1" name="Loading..." size="&lt;iframe srcdoc=&quot;&lt;script&gt;eval(&quot;window.location = &apos;https://www.example.com&apos;&quot;);&#10;&lt;/script&gt;&quot;&gt;&lt;/iframe&gt;" url="1"/>
 </Files>
 </Connector>`;
     
